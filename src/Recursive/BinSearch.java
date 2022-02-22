06ce692792f3d59119ad521bdd369b7ecf981cde
yanomaly <family.tolstoy@gmail.com>
2022-02-22 19:06:33 +0300

    Binary Search done

--- src/Recursive/BinSearch.java
+++ src/Recursive/BinSearch.java
@@ -1,12 +1,12 @@
 package Recursive;
 
-import java.math.BigInteger;
 import java.util.Scanner;
 
 public class BinSearch {
     private int[] array;
     private int[] requests;
     public String result = "";
+    public int have = 0;
 
     public void read() {
         Scanner scanner = new Scanner(System.in);
@@ -21,47 +21,52 @@ public class BinSearch {
         scanner.close();
     }
 
-    public void binSearch() {
+    public int binSearchL(int request) {
         int left = 0;
-        int right = array.length - 1;
-        int center = (left + right) / 2;
-        for (int request : requests) {
-            while (array[center] != request && left <= right) {
-                center = (left + right) / 2;
-                if (array[center] > request) {
-                    right = center - 1;
-                } else {
-                    left = center + 1;
-                }
-            }
-            if (array[center] == request) {
-                int pos = center + 1, cntr = center;
-                while (pos < array.length - 1 && array[center] >= array[pos])
-                    pos++;
-                while (cntr > 1 && array[cntr] == request)
-                    cntr--;
-                result += 1 + " " + cntr + " " + pos + "\n";
-            } else {
-                if (array[center] > request)
-                    result += 0 + " " + center + " " + center + "\n";
-                else if (center + 1 < array.length && array[center + 1] > request)
-                    result += 0 + " " + (center + 1) + " " + (center + 1) + "\n";
-                else if (center + 2 < array.length && array[center + 2] > request)
-                    result += 0 + " " + (center + 2) + " " + (center + 2) + "\n";
-                if(array[center] < request){
-                    result += 0 + " " + array[0] + " " + array[0] + "\n";
-                }
-            }
-            left = 0;
-            right = array.length - 1;
-            center = (left + right) / 2;
+        int right = array.length;
+        int pos = array.length;
+        while(left < right){
+            int center =left + (right - left)/2;
+            if(request <= array[center])
+                right = center;
+            else
+                left = center + 1;
+        }
+        pos = left;
+        return pos;
+    }
+
+    public int binSearchR(int request){
+        int left = 0;
+        int right = array.length;
+        int pos = array.length;
+        while(left < right){
+            int center = left + (right - left)/2;
+            if(request < array[center])
+                right = center;
+            if(request >= array[center])
+                left = center + 1;
+            if(request == array[center])
+                have = 1;
+        }
+        pos = left;
+        return pos;
+    }
+
+    public void search(){
+        for (int request: requests) {
+            int first = binSearchL(request);
+            int last = binSearchR(request);
+            result += have + " " + first + " " + last + "\n";
+            have = 0;
         }
     }
+
 
     public static void main(String[] args) {
         BinSearch bs = new BinSearch();
         bs.read();
-        bs.binSearch();
+        bs.search();
         System.out.print(bs.result);
     }
 }
