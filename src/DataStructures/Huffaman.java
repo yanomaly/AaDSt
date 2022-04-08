b9e009a8720875a83b8cd20b8c7a332d7ca52a41
yanomaly <family.tolstoy@gmail.com>
2022-04-08 21:32:58 +0300

    Huffman encryption done

---
+++ src/DataStructures/Huffaman.java
@@ -0,0 +1,65 @@
+package DataStructures;
+
+import java.io.*;
+import java.util.PriorityQueue;
+
+public class Huffaman {
+
+    private static long length;
+
+    static class Tree{
+        private Node root;
+    }
+
+    static class Node{
+        private long key;
+        private Node LCh;
+        private Node RCh;
+
+        public Node(long key) {
+            this.key = key;
+        }
+
+        public Node(long key, Node LCh, Node RCh) {
+            this.key = key;
+            this.LCh = LCh;
+            this.RCh = RCh;
+        }
+    }
+
+    public static void detour(Node node, long count){
+        if(node.LCh != null) {
+            count++;
+            detour(node.LCh, count);
+            count--;
+        }
+        if(node.RCh != null){
+            count++;
+            detour(node.RCh, count);
+            count--;
+        }
+        if(node.LCh == null && node.RCh == null)
+            length += count * node.key;
+    }
+
+    public static void main(String[] args) throws IOException {
+        BufferedReader reader = new BufferedReader(new FileReader("input.txt"));
+        int size = Integer.parseInt(reader.readLine());
+        Tree tree = new Tree();
+        String arr[] = reader.readLine().split(" ");
+        PriorityQueue<Node> nodes = new PriorityQueue<>((x, y) -> x.key > y.key ? 1 : x.key < y.key ? -1 : 0);
+        for (int i = 0; i < size; i++)
+            nodes.add(new Node(Integer.parseInt(arr[i])));
+        while(nodes.size() > 1){
+            Node min = nodes.poll();
+            Node min2 = nodes.poll();
+            Node next = new Node(min.key + min2.key, min2, min);
+            nodes.add(next);
+            tree.root = next;
+        }
+        Huffaman.detour(tree.root, 0L);
+        try(BufferedWriter writer = new BufferedWriter(new FileWriter("output.txt"))){
+            writer.write(String.valueOf(length));
+        }
+    }
+}
